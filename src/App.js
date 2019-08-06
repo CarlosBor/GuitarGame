39f865f783caa34eb8fc867af22b181757c9774f
CarlosBor <carlosborjawebdev@gmail.com>
2019-08-06 23:55:12 +0200

    Added wire generation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      tuning:["E","B","G","D","A","E"]
+      tuning:["E","B","G","D","A","E"],
+      wireNumber:6,
+      fretNumber:1,
     };
 //    this.handleTuningClick = this.handleTuningClick.bind(this);
   }
@@ -17,14 +19,7 @@ class Game extends React.Component {
     return (
       <div class="game">
           <div class="board">
-            <div>
-                <div>{this.state.tuning[0]}</div>
-                <div>{this.state.tuning[1]}</div>
-                <div>{this.state.tuning[2]}</div>
-                <div>{this.state.tuning[3]}</div>
-                <div>{this.state.tuning[4]}</div>
-                <div>{this.state.tuning[5]}</div>
-            </div>
+              {this.renderFrets(6,4,["E","B","G","D","A","E"])}
           </div>
           <div class="options">
               {this.renderTuning(6)}
@@ -33,9 +28,12 @@ class Game extends React.Component {
       </div>
     );
   }
-
-  renderFret(note){
-    return <Fret value={note} />
+  renderFrets(numberOfFrets,numberOfWires,tuning){
+    return <Board
+      numberOfFrets = {numberOfFrets}
+      numberOfWires = {numberOfWires}
+      tuning = {tuning}
+    />
   }
   renderTuning(wires){
     return <Options 
@@ -43,11 +41,10 @@ class Game extends React.Component {
       onChange={this.handleTuningClick}
     />
   }
-  clicktest(event){
-    console.log("Wohoo");
-    console.log(event.target.value);
-  }
-
+  /* React.Component doesn't auto bind methods to itself. You need to bind them yourself */
+  /* Option A: Bind them in the constructor, as in this.handleTuningClick = this.handleTuningClick.bind(this); in the constructor of this class*/
+  /* Option B: when passed as a prop, pass it as this.handleTuningClick.bind(this) */
+  /* Option C: What is displayed here, define the function as an event that executes a function ¿?*/
   handleTuningClick = event => {
     var target = event.target.value.split(",")[0];
     var tunedNote = event.target.value.split(",")[1];
@@ -57,39 +54,51 @@ class Game extends React.Component {
         tuning : newTuning
       }
     )
-/*
-    this.setState(
-      state =>{
-        var list = state.list.map((element, j) => {
-          if (j===target){
-            return tunedNote;
-          }else{
-            return element;
-          }
-        })
-      });
-      */
   }
 }
 
-class Fret extends React.Component{
+class Board extends React.Component{
+  renderWire(numberOfFrets, wireTuning){
+    var notesUsed = [];
+    var fretNodes = [];
+    for (var i=0;i<numberOfFrets;i++){
+      notesUsed[i] = calculateNote(wireTuning,i);
+    }
+    fretNodes = notesUsed.map((note) =>
+      <div class="fret" value={note}></div>
+    );
+    return(
+      <div class="wire">
+        {fretNodes}
+      </div>
+    )
+  }
+
+  renderBoard(numberOfFrets, numberOfWires, tuning){
+ //Go for horizontal divs filled with frets instead of columns
+    var wires = [];
+    for (var i=0;i<numberOfWires;i++){
+      wires[i] = this.renderWire(numberOfFrets,tuning[i]);
+    }
+    return(
+     <>{wires}</>
+    )
+  }
+
   render(){
-    return (
-      <div className="fret">{this.props.value}</div>
+    return(
+      <div class="fretBoard">
+      {this.renderBoard(this.props.numberOfFrets, this.props.numberOfWires,this.props.tuning)}
+      </div>
     )
   }
 }
 
 class Options extends React.Component{
 
-  /*
-  setTuning(event){
-    console.log(event.target.value);
-  }*/
-
   //Maps from the notes array to make 12 <option> nodes, then from that to make the desired amount of <select> nodes with <option> in them
   generateTuningDropdowns(wireNumber){
-    var allNotes = []
+    var allNotes = [];
     for(var i=0;i<wireNumber;i++){
       allNotes[i] = noteCircle.map((notes)=>
        <option value = {i+","+notes}>{notes}</option>
@@ -124,6 +133,7 @@ const noteCircle = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "
 
 //I need a function that adds a line of divs to div.board, getting their note from their left or from options if there isn't any.
 function calculateNote(initialNote, stepNumber){
+  var noteCircle = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
     //Given a initial note and a number of steps, it returns the note X steps from the initial note.
     initialNote = initialNote.toUpperCase();
     stepNumber = stepNumber + findNote(initialNote);
